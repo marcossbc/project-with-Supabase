7db5475737eeb72dfe9ccf904b9b432ed50b6654
marcossbc <marcossbc880@gmail.com>
2025-10-15 09:15:15 +0300

    added UseStatate

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
 
 function SignUp() {
+  const [ email, setEmail] = useState("")
+  const [ usarname, setUsarname] = useState("")
+  const [ password, setPassword] = useState("")
+  const [ confirmPassword, setConfirmPassword] = useState("")
+  const [ isLoading, setLoading] = useState(false)
+  const [ error, setError] = useState(null)
+  const [ success, setSuccess] = useState(false)
+
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-50 px-6'>
       <div className='max-w-md '>
@@ -10,6 +19,9 @@ function SignUp() {
         </div>
         {/* info */}
         <div className='bg-white rounded-lg shadow-md p-7'>
+          <form >
+
+          </form>
 
         </div>
       </div>
