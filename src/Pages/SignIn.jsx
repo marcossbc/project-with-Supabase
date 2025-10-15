b94f15ee34f5f6ca64a1f8ca09a3263a3307ca87
marcossbc <marcossbc880@gmail.com>
2025-10-15 09:51:08 +0300

    adde page sing in

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 function SignIn() {
+    const [email, setEmail] = useState('')
+
+  const [password, setPassword] = useState('')
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
   return (
     <div>
       <h1>hellow </h1>
