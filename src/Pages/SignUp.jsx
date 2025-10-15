07db01c086b07a14e55486d3d1fb6efbec1dab8a
marcossbc <marcossbc880@gmail.com>
2025-10-15 09:07:23 +0300

    create singup

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>
+    <div className='min-h-screen flex items-center justify-center bg-gray-50 px-6'>
+      <div className='max-w-md '>
+        <div>
+          <h1>Create Account</h1>
+          <p>join communty  and start shering your ideas</p>
+        </div>
+        {/* info */}
+        <div className='bg-white rounded-lg shadow-md p-7'>
+
+        </div>
+      </div>
       
     </div>
   )
