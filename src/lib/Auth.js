b13d755d24a6cb654bb25c6f24900b22b6beef06
marcossbc <marcossbc880@gmail.com>
2025-10-16 18:31:51 +0300

    stup date auth

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -7,4 +7,46 @@ export async function SignUpPage(email, password, username = "") {
         data: { hello: 'world' }
     })
 
+    
+      if(data?.user) {
+        // hduu session jiro nasii 
+
+        const { data : sessionData } = await supabase.auth.getSession()
+
+        if(!sessionData?.session) {
+            // hdii uusan jirin session 
+            console.log('No active session yet - profile will be created on first sign in')
+            // date nosoo celi 
+            return data;
+        }
+    
+     // qeybta magaca username text avater_url
+      const displayName = username || email.split("@")[0];
+
+    //   create profile
+
+    const {data: profileData, error : profileError } = await supabase
+    
+        .from('users')
+        .insert({
+            id: data.user.id,
+            username: displayName,
+            avatar_url: null
+        })
+        .select()
+        .single()
+
+        if(profileError){
+            console.error("profile creation error:", profileError)
+        }else{
+            console.log("Profile created successfully", profileData)
+        }
+
+    }
+
+    return data
+
+
+
 }
+
