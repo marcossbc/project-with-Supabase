ca38bc43cc27cef8d4025f377cbf7e428fba3bfa
marcossbc <marcossbc880@gmail.com>
2025-10-16 17:18:05 +0300

    added adate  error

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -1,7 +1,7 @@
 import supabase from "./Supabase";
 
 export async function SignUpPage(email, password, username = "") {
-    const data = await supabase.auth.updateUser({
+    const {data, error} = await supabase.auth.updateUser({
         email: email,
         password:password,
         data: { hello: 'world' }
