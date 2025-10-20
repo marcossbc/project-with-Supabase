eaeb1c363c1e53533bf09c01316ea19d115e2c7d
marcossbc <marcossbc880@gmail.com>
2025-10-20 08:18:37 +0300

    articales edite page

--- src/Pages/ArticleEditorPage.jsx
+++ src/Pages/ArticleEditorPage.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { useAuth } from '../context/AuthContext'
 
 function ArticleEditorPage() {
+   const isEditMode = Boolean(id)
+
+    // State for article data
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('') // Always initialize as an empty string
+    const [selectedTags, setSelectedTags] = useState([])
+    const [isSaving, setIsSaving] = useState(false)
+    const [isTagsMenuOpen, setIsTagsMenuOpen] = useState(false)
+    const [featuredImageUrl, setFeaturedImageUrl] = useState('')
+    const [isPublished, setIsPublished] = useState(false)
+    const [error, setError] = useState(null)
+
+    // State for image upload
+    const [selectedImage, setSelectedImage] = useState(null)
+    const [isUploading, setIsUploading] = useState(false)
+    const [imagePath, setImagePath] = useState('')
+
+    const { user } = useAuth()
+    const navigate = useNavigate()
+
+
+
   return (
-    <div>
-      
-    </div>
+    
   )
 }
 
